e3ced0e59d0bea0a493ab19c98672c30e4a51f9e
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-03-22 11:21:43 -0300

    Aula 7

---
+++ Aula 7/Programa.java
@@ -0,0 +1,10 @@
+public class Programa {
+    public static void main(String[] args) {
+        int numero = 100;
+        String numTexto = String.valueOf(numero);
+
+        char primeiraLetra = numTexto.charAt(0);
+        System.out.println(primeiraLetra);
+        
+    }
+}
