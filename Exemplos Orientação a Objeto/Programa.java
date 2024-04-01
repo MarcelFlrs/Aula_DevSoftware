83878730bfce36eea4e48209ae73690f3a967e0f
Marcel <127535882+MarcelFlrs@users.noreply.github.com>
2024-04-01 15:16:45 -0300

    Update Programa.java

--- Exemplos Orientação a Objeto/Programa.java
+++ Exemplos Orientação a Objeto/Programa.java
@@ -1,16 +1,11 @@
-
 public class Programa {
-
     public static void main(String[] args) {
-
-        Carro uno = new Carro();
-        // uno.velocidade = -10;
-        uno.acelerar();
-        uno.acelerar();
-        uno.acelerar();
-        uno.acelerar();
-        uno.acelerar();
-
+    Carro uno = new Carro();
+    // uno.velocidade = -10;
+    uno.acelerar();
+    uno.acelerar();
+    uno.acelerar();
+    uno.acelerar();
+    uno.acelerar();
     }
-
 }
