3192621adc47587326aaaa08b1656828a1e3c49c
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-04-12 09:34:35 -0300

    Restaurante

---
+++ Exemplo Restaurante/src/br/edu/up/modelos/Pedido.java
@@ -0,0 +1,15 @@
+package br.edu.up.modelos;
+
+//O pedido é de um cliente
+public class Pedido {
+
+    // O cliente do pedido
+    Cliente cliente;
+
+    // O pedido foi realizado pelo garçom
+    Garcom garcom;
+
+    // O garçom criar lista de itens do pedido
+    Item[] itens;
+
+}
